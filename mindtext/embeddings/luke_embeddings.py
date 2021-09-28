6b2be65b30c15275c0003eb1ebb6ad23ea4b9904
Hongye <470141216@qq.com>
2021-09-28 10:19:36 +0800

    update luke model and verify model's outputs
    no message
    
    no message
    
    no message
    
    no message

--- mindtext/embeddings/luke_embeddings.py
+++ mindtext/embeddings/luke_embeddings.py
@@ -15,8 +15,6 @@
 """
     luke embeddings
 """
-from mindtext.modules.encoder.bert import EmbeddingLookup, EmbeddingPostprocessor
-from mindspore.common.initializer import initializer
 import mindspore.ops as ops
 import mindspore.nn as nn
 import mindspore.common.dtype as mstype
@@ -24,78 +22,143 @@ import mindspore.common.dtype as mstype
 
 class EntityEmbeddings(nn.Cell):
     """entity embeddings for luke model"""
+
     def __init__(self, config):
         super(EntityEmbeddings, self).__init__()
-        self.config = config
-        config.entity_vocab_size = 20
-        config.entity_emb_size = config.hidden_size
-        config.layer_norm_eps = 1e-6
+        self.entity_emb_size = config.entity_emb_size
+        self.hidden_size = config.hidden_size
+        self.entity_embeddings = nn.Embedding(config.entity_vocab_size, config.entity_emb_size, padding_idx=0)
 
-        self.entity_embeddings = nn.Embedding(config.entity_vocab_size, config.entity_emb_size)
+        if config.entity_emb_size != config.hidden_size:
+            self.entity_embedding_dense = nn.Dense(config.entity_emb_size, config.hidden_size, has_bias=False)
 
         self.position_embeddings = nn.Embedding(config.max_position_embeddings, config.hidden_size)
         self.token_type_embeddings = nn.Embedding(config.type_vocab_size, config.hidden_size)
 
-        self.layer_norm = nn.LayerNorm((config.hidden_size, config.hidden_size), epsilon=config.layer_norm_eps)
+        self.layer_norm = nn.LayerNorm([config.hidden_size], epsilon=config.layer_norm_eps)
         self.dropout = nn.Dropout(config.hidden_dropout_prob)
         self.unsqueezee = ops.ExpandDims()
 
     def construct(self, entity_ids, position_ids, token_type_ids=None):
         """EntityEmbeddings for luke"""
+
         if token_type_ids is None:
             token_type_ids = ops.zeros_like(entity_ids)
 
         entity_embeddings = self.entity_embeddings(entity_ids)
+        if self.entity_emb_size != self.hidden_size:
+            entity_embeddings = self.entity_embedding_dense(entity_embeddings)
+        entity_position_ids_int = clamp(position_ids)
 
-        position_embeddings = self.position_embeddings(clamp(position_ids))
-        position_embedding_mask = self.unsqueezee((position_ids != -1), -1)
+        position_embeddings = self.position_embeddings(entity_position_ids_int)
+
+        position_ids = position_ids.astype(mstype.int32)
+        position_embedding_mask = 1.0 * self.unsqueezee((position_ids != -1), -1)
+
         position_embeddings = position_embeddings * position_embedding_mask
+
         position_embeddings = ops.reduce_sum(position_embeddings, -2)
         position_embeddings = position_embeddings / clamp(ops.reduce_sum(position_embedding_mask, -2), minimum=1e-7)
 
         token_type_embeddings = self.token_type_embeddings(token_type_ids)
 
         embeddings = entity_embeddings + position_embeddings + token_type_embeddings
+
         embeddings = self.layer_norm(embeddings)
         embeddings = self.dropout(embeddings)
-
         return embeddings
 
 
-def clamp(x, minimum=0.0):
+def clamp(x, minimum=0):
     mask = x > minimum
     x = x * mask + minimum
     return x
 
 
 class RobertaEmbeddings(nn.Cell):
-    """RoBERTa Embedding for luke """
+    """
+    RoBERTaEmbeddings
+    """
+
     def __init__(self, config):
-        output_embedding_shape = [config.batch_size, config.seq_length,
-                                  config.embedding_size]
         super(RobertaEmbeddings, self).__init__()
-        self.bert_embedding_lookup = EmbeddingLookup(
-            vocab_size=config.vocab_size,
-            embedding_size=self.embedding_size,
-            embedding_shape=output_embedding_shape,
-            use_one_hot_embeddings=False,
-            initializer_range=config.initializer_range)
+        self.word_embeddings = nn.Embedding(config.vocab_size,
+                                            config.hidden_size,
+                                            padding_idx=config.pad_token_id
+                                            )
+        self.position_embeddings = nn.Embedding(config.max_position_embeddings,
+                                                config.hidden_size)
+        self.token_type_embeddings = nn.Embedding(config.type_vocab_size,
+                                                  config.hidden_size)
 
-        self.bert_embedding_postprocessor = EmbeddingPostprocessor(
-            embedding_size=self.embedding_size,
-            embedding_shape=output_embedding_shape,
-            use_relative_positions=config.use_relative_positions,
-            use_token_type=True,
-            token_type_vocab_size=config.type_vocab_size,
-            use_one_hot_embeddings=False,
-            initializer_range=0.02,
-            max_position_embeddings=config.max_position_embeddings,
-            dropout_prob=config.hidden_dropout_prob)
-        self.token_type_ids = initializer(
-            "zeros", [self.batch_size, self.seq_length], mstype.int32).to_tensor()
+        # self.LayerNorm is not snake-cased to stick with TensorFlow model variable name and be able to load
+        # any TensorFlow checkpoint file
+        self.layer_norm = nn.LayerNorm([config.hidden_size],
+                                       epsilon=config.layer_norm_eps)
+        self.dropout = nn.Dropout(config.hidden_dropout_prob)
+        self.position_embedding_type = getattr(config, "position_embedding_type", "absolute")
 
-    def construct(self, input_ids):
-        word_embeddings, _ = self.bert_embedding_lookup(input_ids)
-        embedding_output = self.bert_embedding_postprocessor(self.token_type_ids,
-                                                             word_embeddings)
-        return embedding_output
+        # End copy
+        self.padding_idx = config.pad_token_id
+        self.position_embeddings = nn.Embedding(config.max_position_embeddings,
+                                                config.hidden_size,
+                                                padding_idx=self.padding_idx)
+
+    def construct(self,
+                  input_ids=None,
+                  token_type_ids=None,
+                  position_ids=None,
+                  inputs_embeds=None,
+                  past_key_values_length=0):
+        """
+        Returns the result of the model after loading the pre-training weights
+
+        Args:
+            input_ids:A vector containing the transformation of characters into corresponding ids.
+            token_type_ids:A vector containing segemnt ids.
+            position_ids:A vector containing position_ids.
+            inputs_embeds:A vector containing embeds
+            past_key_values_length: A number
+
+        Returns:
+            RoBERTa embeddings
+        """
+        if position_ids is None:
+            if input_ids is not None:
+                position_ids = create_position_ids_from_input_ids(input_ids, self.padding_idx, past_key_values_length)
+            else:
+                position_ids = create_position_ids_from_input_ids(inputs_embeds)
+
+        input_shape = input_ids.shape
+        if token_type_ids is None:
+            token_type_ids = ops.Zeros(input_shape, dtype=mstype.int64)
+        if inputs_embeds is None:
+            inputs_embeds = self.word_embeddings(input_ids)
+        token_type_embeddings = self.token_type_embeddings(token_type_ids)
+
+        embeddings = inputs_embeds + token_type_embeddings
+        position_ids = position_ids.astype(mstype.int32)
+        position_embeddings = self.position_embeddings(position_ids)
+        embeddings += position_embeddings
+        embeddings = self.layer_norm(embeddings)
+        embeddings = self.dropout(embeddings)
+        return embeddings
+
+
+def create_position_ids_from_input_ids(input_ids, padding_idx=0, past_key_values_length=0):
+    """
+    Replace non-padding symbols with their position numbers. Position numbers begin at padding_idx+1. Padding symbols
+    are ignored. This is modified from fairseq's `utils.make_positions`.
+    Args:
+       input_ids:
+       padding_idx:
+       past_key_values_length:
+    Returns:
+        Tensor
+    """
+    mask = input_ids != padding_idx
+    mask = (1.0 * mask)
+
+    cumsum = ops.CumSum()
+    incremental_indices = (cumsum(mask, 1) + past_key_values_length) * mask
+    return incremental_indices + padding_idx
--- mindtext/tagging/models/luke.py
+++ mindtext/tagging/models/luke.py
@@ -15,18 +15,100 @@
 """
     luke for tagging and reading comprehension tasks
 """
+from typing import Optional
+
 import mindspore.nn as nn
 import mindspore.ops as ops
+from mindspore.ops import operations as P
+from mindspore.ops import functional as F
+from mindspore.ops import composite as C
+from mindspore.common.tensor import Tensor
+from mindspore.common.parameter import Parameter
+from mindspore.common import dtype as mstype
+from mindspore.nn.wrap.grad_reducer import DistributedGradReducer
+from mindspore.context import ParallelMode
+from mindspore.communication.management import get_group_size
+from mindspore import context
 from mindtext.modules.encoder.luke import LukeEntityAwareAttentionModel
 
+_grad_overflow = C.MultitypeFuncGraph("_grad_overflow")
+GRADIENT_CLIP_TYPE = 1
+GRADIENT_CLIP_VALUE = 1.0
+grad_scale = C.MultitypeFuncGraph("grad_scale")
+reciprocal = P.Reciprocal()
+clip_grad = C.MultitypeFuncGraph("clip_grad")
 
-class LukeForReadingComprehension(LukeEntityAwareAttentionModel):
+
+@clip_grad.register("Number", "Number", "Tensor")
+def _clip_grad(clip_type, clip_value, grad):
+    """
+    Clip gradients.
+
+    Inputs:
+        clip_type (int): The way to clip, 0 for 'value', 1 for 'norm'.
+        clip_value (float): Specifies how much to clip.
+        grad (tuple[Tensor]): Gradients.
+
+    Outputs:
+        tuple[Tensor], clipped gradients.
+    """
+    if clip_type not in (0, 1):
+        return grad
+    dt = F.dtype(grad)
+    if clip_type == 0:
+        new_grad = C.clip_by_value(grad, F.cast(F.tuple_to_array((-clip_value,)), dt),
+                                   F.cast(F.tuple_to_array((clip_value,)), dt))
+    else:
+        new_grad = nn.ClipByNorm()(grad, F.cast(F.tuple_to_array((clip_value,)), dt))
+    return new_grad
+
+
+class LukeForReadingComprehension(nn.Cell):
     """Luke for reading comprehension task"""
-    def __init__(self, config):
-        super(LukeForReadingComprehension, self).__init__(config)
 
-        self.qa_outputs = nn.Dense(self.config.hidden_size, 2)
+    def __init__(self, config):
+        super(LukeForReadingComprehension, self).__init__()
+        self.luke = LukeEntityAwareAttentionModel(config)
+        self.qa_outputs = nn.Dense(config.hidden_size, 2)
         self.split = ops.Split(-1, 2)
+        self.squeeze = ops.Squeeze(-1)
+        self.shape = ops.Shape()
+
+    def construct(
+            self,
+            word_ids,
+            word_segment_ids,
+            word_attention_mask,
+            entity_ids,
+            entity_position_ids,
+            entity_segment_ids,
+            entity_attention_mask
+    ):
+        """LukeForReadingComprehension construct"""
+        encoder_outputs = self.luke(
+            word_ids,
+            word_segment_ids,
+            word_attention_mask,
+            entity_ids,
+            entity_position_ids,
+            entity_segment_ids,
+            entity_attention_mask,
+        )
+
+        word_hidden_states = encoder_outputs[0][:, : ops.shape(word_ids)[1], :]
+        logits = self.qa_outputs(word_hidden_states)
+        start_logits, end_logits = self.split(logits)
+        start_logits = self.squeeze(start_logits)
+        end_logits = self.squeeze(end_logits)
+        return start_logits, end_logits
+
+
+class LukeForReadingComprehensionWithLoss(nn.Cell):
+    """LukeForReadingComprehensionWithLoss"""
+
+    def __init__(self, net, loss):
+        self.net = net
+        self.loss = loss
         self.squeeze = ops.Squeeze(-1)
 
     def construct(
@@ -38,31 +120,145 @@ class LukeForReadingComprehension(LukeEntityAwareAttentionModel):
             entity_position_ids,
             entity_segment_ids,
             entity_attention_mask,
+            start_positions=None,
+            end_positions=None
     ):
-        """LukeForReadingComprehension construct"""
-        encoder_outputs = super(LukeForReadingComprehension, self).forward(
-            word_ids,
-            word_segment_ids,
-            word_attention_mask,
-            entity_ids,
-            entity_position_ids,
-            entity_segment_ids,
-            entity_attention_mask,
-        )
+        """
+        LukeForReadingComprehensionWithLoss's construct
+        """
+        start_logits, end_logits = self.net(word_ids,
+                                            word_segment_ids,
+                                            word_attention_mask,
+                                            entity_ids,
+                                            entity_position_ids,
+                                            entity_segment_ids,
+                                            entity_attention_mask)
+        if start_positions is not None and end_positions is not None:
+            if len(start_positions.size()) > 1:
+                start_positions = self.squeeze(start_positions)
+            if len(end_positions.size()) > 1:
+                end_positions = self.squeeze(end_positions)
 
-        word_hidden_states = encoder_outputs[0][:, : ops.shape(word_ids), :]
-        logits = self.qa_outputs(word_hidden_states)
-        start_logits, end_logits = self.split(logits)
-        start_logits = self.squeeze(start_logits)
-        end_logits = self.squeeze(end_logits)
+            ignored_index = start_logits.size(1)
 
-        return start_logits, end_logits
+            start_positions.clamp_(0, ignored_index)
+            end_positions.clamp_(0, ignored_index)
+
+            start_loss = self.loss(start_logits, start_positions)
+            end_loss = self.loss(end_logits, end_positions)
+            total_loss = (start_loss + end_loss) / 2
+            outputs = (total_loss,)
+        else:
+            outputs = tuple()
+
+        return outputs + (start_logits, end_logits,)
 
 
-if __name__ == "__main__":
-    LukeForReadingComprehension(config='')
-    # config = LukeConfig("config.json")
-    # x = LukeForReadingComprehension(config)
-    #
-    # param_dict = load_checkpoint("luke.ckpt")
-    # load_param_into_net(x, param_dict)
+class LukeSquadCell(nn.Cell):
+    """
+    LukeSquadCell for Train
+    """
+    def __init__(self, network, optimizer, scale_update_cell=None):
+        super(LukeSquadCell, self).__init__(auto_prefix=False)
+        self.network = network
+        self.network.set_grad()
+        self.weights = optimizer.parameters
+        self.optimizer = optimizer
+        self.grad = C.GradOperation(get_by_list=True, sens_param=True)
+        self.reducer_flag = False
+        self.allreduce = P.AllReduce()
+        self.parallel_mode = context.get_auto_parallel_context("parallel_mode")
+        if self.parallel_mode in [ParallelMode.DATA_PARALLEL, ParallelMode.HYBRID_PARALLEL]:
+            self.reducer_flag = True
+        self.grad_reducer = None
+        if self.reducer_flag:
+            mean = context.get_auto_parallel_context("gradients_mean")
+            degree = get_group_size()
+            self.grad_reducer = DistributedGradReducer(optimizer.parameters, mean, degree)
+        self.is_distributed = (self.parallel_mode != ParallelMode.STAND_ALONE)
+        self.cast = P.Cast()
+        self.gpu_target = False
+        if context.get_context("device_target") == "GPU":
+            self.gpu_target = True
+            self.float_status = P.FloatStatus()
+            self.addn = P.AddN()
+            self.reshape = P.Reshape()
+        else:
+            self.alloc_status = P.NPUAllocFloatStatus()
+            self.get_status = P.NPUGetFloatStatus()
+            self.clear_status = P.NPUClearFloatStatus()
+        self.reduce_sum = P.ReduceSum(keep_dims=False)
+        self.base = Tensor(1, mstype.float32)
+        self.less_equal = P.LessEqual()
+        self.hyper_map = C.HyperMap()
+        self.loss_scale = None
+        self.loss_scaling_manager = scale_update_cell
+        if scale_update_cell:
+            self.loss_scale = Parameter(Tensor(scale_update_cell.get_loss_scale(), dtype=mstype.float32))
+
+    def construct(self,
+                  word_ids,
+                  word_segment_ids,
+                  word_attention_mask,
+                  entity_ids,
+                  entity_position_ids,
+                  entity_segment_ids,
+                  entity_attention_mask,
+                  start_positions,
+                  end_positions,
+                  sens: Optional[int] = None
+                  ):
+        """LukeSquad"""
+        weights = self.weights
+        init = False
+        loss = self.network(word_ids,
+                            word_segment_ids,
+                            word_attention_mask,
+                            entity_ids,
+                            entity_position_ids,
+                            entity_segment_ids,
+                            entity_attention_mask,
+                            start_positions,
+                            end_positions)
+        if sens is None:
+            scaling_sens = self.loss_scale
+        else:
+            scaling_sens = sens
+        if not self.gpu_target:
+            init = self.alloc_status()
+            init = F.depend(init, loss)
+            clear_status = self.clear_status(init)
+            scaling_sens = F.depend(scaling_sens, clear_status)
+        grads = self.grad(self.network, weights)(word_ids,
+                                                 word_segment_ids,
+                                                 word_attention_mask,
+                                                 entity_ids,
+                                                 entity_position_ids,
+                                                 entity_segment_ids,
+                                                 entity_attention_mask,
+                                                 start_positions,
+                                                 end_positions)
+        grads = self.hyper_map(F.partial(grad_scale, scaling_sens), grads)
+        grads = self.hyper_map(F.partial(clip_grad, GRADIENT_CLIP_TYPE, GRADIENT_CLIP_VALUE), grads)
+        if self.reducer_flag:
+            grads = self.grad_reducer(grads)
+        if not self.gpu_target:
+            init = F.depend(init, grads)
+            get_status = self.get_status(init)
+            init = F.depend(init, get_status)
+            flag_sum = self.reduce_sum(init, (0,))
+        else:
+            flag_sum = self.hyper_map(F.partial(_grad_overflow), grads)
+            flag_sum = self.addn(flag_sum)
+            flag_sum = self.reshape(flag_sum, (()))
+        if self.is_distributed:
+            flag_reduce = self.allreduce(flag_sum)
+            cond = self.less_equal(self.base, flag_reduce)
+        else:
+            cond = self.less_equal(self.base, flag_sum)
+        overflow = cond
+        if sens is None:
+            overflow = self.loss_scaling_manager(self.loss_scale, cond)
+        if not overflow:
+            self.optimizer(grads)
+        return (loss, cond)
